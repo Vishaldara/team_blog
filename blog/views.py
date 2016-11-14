b9178a2ac545da72f5972b556385bd3394926e41
Vishal Dara <Vishal@Apples-MacBook-Pro.local>
2016-11-14 11:48:28 +0000

    First Working Blog Prototype

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,7 @@ from models import Post
 # Create your views here.
 def listofposts(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request ,"listsofposts.html",{"posts":posts})
+    return render(request ,"listofposts.html",{"posts":posts})
 
+
+def postdetail(request):
