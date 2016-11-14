813e82d19c9c77bec8fb88412c528b041ed6b0b0
Vishal Dara <Vishal@Apples-MacBook-Pro.local>
2016-11-14 11:21:40 +0000

    basic wiring in place, urls, views, model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey("auth.User")
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
+    count = models.IntegerField(default=0)
+
+
+    def published(self):
+        self.published_date=timezone.now()
+        self.save()
+
+
+
+
+
